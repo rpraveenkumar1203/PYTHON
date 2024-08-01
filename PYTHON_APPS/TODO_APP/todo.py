162e00efd074fb8693cb4552167d4afe6d631c97
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-01 23:25:38 +0530

    day1 todo PYTHON_APPS

--- PYTHON_APPS/TODO_APP/todo.py
+++ PYTHON_APPS/TODO_APP/todo.py
@@ -1,6 +1,8 @@
 user_prompt = 'Enter a todo !'
 
-todo1,todo2,todo3 = input(user_prompt)
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
 
 todo_s_List = [todo1,todo2,todo3]
 
