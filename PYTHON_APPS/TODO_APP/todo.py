35fce2195640e4f8b1cae44fb3968671572b6f93
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-01 23:30:15 +0530

    added inline comments

--- PYTHON_APPS/TODO_APP/todo.py
+++ PYTHON_APPS/TODO_APP/todo.py
@@ -1,11 +1,17 @@
+
+# assiging a variable to user prompt
 user_prompt = 'Enter a todo !'
 
+# storing the user input
 todo1 = input(user_prompt)
 todo2 = input(user_prompt)
 todo3 = input(user_prompt)
 
+# storing the all the user input in single list
 todo_s_List = [todo1,todo2,todo3]
 
+
+# printing the outut and it type 
 print(todo_s_List)
 print(type(todo_s_List))
 
