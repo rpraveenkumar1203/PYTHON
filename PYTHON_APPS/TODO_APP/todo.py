cab946867c3b3da611dffc62713be843f290ab86
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-02 23:11:27 +0530

    upgraded the app with match case statement with proper user prompts

--- PYTHON_APPS/TODO_APP/todo.py
+++ PYTHON_APPS/TODO_APP/todo.py
@@ -1,12 +1,23 @@
-# prompt message variable 
-user_input = "Enter the todo ! "
+todos =[]
 
-#declaring the list to store all the todos 
+# intiating the loop 
 
-todos = []
-
-#implementing the while loop 
 while True:
-    todo = input(user_input)
-    todos.append(todo)
-    print(todos)
+    user_action = input("Enter add or show or exit")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo action : ")
+            todo = todo.title()
+            todos.append(todo)
+        case 'show' | 'display':
+            for items in todos:
+                print(items)
+        case 'exit':
+            break                
+        case _ :
+            print('Enter show or add or display or exit the program')
+print('Bye')
+
+
