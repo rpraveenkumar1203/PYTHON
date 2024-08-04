2c255ebab82af252a96ae432b0baa606b541ef57
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-04 19:33:26 +0530

    enhanced edit and completed option

--- PYTHON_APPS/TODO_APP/todo.py
+++ PYTHON_APPS/TODO_APP/todo.py
@@ -1,47 +1,72 @@
 while True:
+
     user_input=input("Do you need a ADD show edit done or exit : \n")
     user_input= user_input.strip()
 
+    
+
     match user_input:
+
         case 'add':
 
             user_input = input("Enter the todo needed to added : \n") + "\n"
 
-            file = open('todos.txt','r')
-            todos = file.readlines()
-            file.close()
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
 
             todos.append(user_input)
-
-            file = open('todos.txt','w')
-            todos = file.writelines(todos)
-            file.close()
-
-
-
+                
+            with open('todos.txt','w') as file:
+                todos = file.writelines(todos)
+                
         case 'show':
 
-            file = open('todos.txt','r')
-            todos = file.readlines()
-            file.close()
-
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
+                
             for index,todo in enumerate(todos):
                 print(f"{index + 1}-{todo}")
 
+
         case 'edit':
+
             number = int(input("Enter a number of which todo to be edited : \n "))
             number = number -1
-            todo = input('enter a new todo : \n')
-            todos[number] = todo
+            
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
+            
+            new_todo = input('enter a new todo : \n')
+            todos[number] = new_todo + "\n"
+
+            with open ('todos.txt','w') as file:
+                file.writelines(todos)
+
 
         case 'done':
-            number = int(input("Enter a number of which todo to be mark as completed : \n"))
-            todos.pop(number-1)
+
+            number = int(input("Enter a number of which todo to be mark as completed : "))
+            with open('todos.txt','r') as file:
+                todos = file.readlines()
+            
+            index = number-1
+
+            print(f"{todos[index]} is marked as completed ")
+            
+            todos.pop(index)
+
+            with open('todos.txt','w') as file:
+                file.writelines(todos)
+            
+            
+                
 
         case 'exit':
+
             break
         
         case _:
+
             print('you have entered a invalid parameter ,Please enter add remove edit or exit ')
 
 print("bye")
