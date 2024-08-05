6caded99e3ad159348ea30c2acbbd4519a0e9352
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-05 22:27:41 +0530

    upgrade with if else

--- PYTHON_APPS/TODO_APP/todo.py
+++ PYTHON_APPS/TODO_APP/todo.py
@@ -1,70 +1,67 @@
 while True:
+    user_input = input("Do you need to add , show , edit , complete ,or exit the todo app : ")
+    user_input = user_input.strip()
 
-    user_input=input("Do you need a ADD show edit done or exit : \n")
-    user_input= user_input.strip()
+    if 'add' in user_input:
 
+        todo = user_input[4:] 
+
+        with open('todos.txt','r') as file:
+            todos = file.readlines()
+
+        todos.append(todo + '\n')
+
+        with open('todos.txt','w') as file:
+            file.writelines(todos)
     
+    elif 'show' in user_input:
 
-    match user_input:
-
-        case 'add':
-
-            user_input = input("Enter the todo needed to added : \n") + "\n"
-
-            with open('todos.txt','r') as file:
-                todos = file.readlines()
-
-            todos.append(user_input)
-                
-            with open('todos.txt','w') as file:
-                todos = file.writelines(todos)
-                
-        case 'show':
-
-            with open('todos.txt','r') as file:
-                todos = file.readlines()
-                
-            for index,todo in enumerate(todos):
-                print(f"{index + 1}-{todo}")
-
-
-        case 'edit':
-
-            number = int(input("Enter a number of which todo to be edited : \n "))
-            number = number -1
-            
-            with open('todos.txt','r') as file:
-                todos = file.readlines()
-            
-            new_todo = input('enter a new todo : \n')
-            todos[number] = new_todo + "\n"
-
-            with open ('todos.txt','w') as file:
-                file.writelines(todos)
-
-
-        case 'done':
-
-            number = int(input("Enter a number of which todo to be mark as completed : "))
-            with open('todos.txt','r') as file:
-                todos = file.readlines()
-            
-            index = number-1
-
-            print(f"{todos[index]} is marked as completed ")
-            
-            todos.pop(index)
-
-            with open('todos.txt','w') as file:
-                file.writelines(todos)         
-                            
-
-        case 'exit':
-
-            break
+        with open('todos.txt','r') as file:
+            todos = file.readlines()
         
-        case _:
+        for index, todo in enumerate(todos):
+            todos = f"{index +1}-{todo}"
+            print(todos)
+            
+    elif 'edit' in user_input:
 
-            print('you have entered a invalid parameter ,Please enter add remove edit or exit ')
+        item_num = int(input("Enter number of which Todo should be Edited :"))
+        item_num = item_num -1
 
-print("bye")
+        updated_todo = input('Enter the new todo : ')
+
+        with open('todos.txt','r') as file:
+            todos = file.readlines()
+        
+        todos[item_num] = updated_todo + '\n'
+
+        with open('todos.txt','w') as file:
+            file.writelines(todos)
+
+    elif 'complete' in user_input:
+
+        user_input = int(input("Enter a number of the todo , which to be marked as complete :"))
+        user_input = user_input - 1 
+
+        with open('todos.txt','r') as file :
+            todos = file.readlines()
+
+        todos.pop(user_input)
+
+        with open('todos.txt','w') as file:
+            file.writelines(todos)
+
+    elif 'exit' in user_input:
+        print("Have a Nice day ...!")
+        break
+
+    else:
+        print('invalid input ...!')
+
+
+
+        
+
+
+
+     
