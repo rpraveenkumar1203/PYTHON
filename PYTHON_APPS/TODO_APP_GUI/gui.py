b22599acf66300d7e8bd9e2bb1be3d93a809c00d
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-10 19:29:34 +0530

    ADDED EDIT BUTTION

--- PYTHON_APPS/TODO_APP_GUI/gui.py
+++ PYTHON_APPS/TODO_APP_GUI/gui.py
@@ -4,6 +4,7 @@ from modules import functions
 label = gui.Text("TODO")
 
 input_box = gui.InputText(tooltip="Enter your Todo",key='todo')
+
 list_box = gui.Listbox(values= functions.read_todos('todos.txt'),key='todos',enable_events=True,size=[45,10])
 
 #Buttons
@@ -25,17 +26,22 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            #app_window['todos'].update(values= [todo.strip() for todo in todos])
             app_window['todos'].update(values=todos)
 
 
         case "Edit":
             todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            new_todo = values['todo'] + "\n"
             todos = functions.get_todos()
             index = todos.index(todo_to_edit)
-            todos[index] = new_todo
+            todos[index] = new_todo 
             functions.write_todos(todos)
             app_window['todos'].update(values=todos)
+
+        case 'todos':
+
+            app_window['todo'].update(value=values['todos'][0])
 
 
             
