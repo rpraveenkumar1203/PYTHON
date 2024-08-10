f05bef8e4f3efc774cee85c88044d711c6f353ae
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-10 19:05:31 +0530

    caevtes cleard

--- PYTHON_APPS/TODO_APP_GUI/gui.py
+++ PYTHON_APPS/TODO_APP_GUI/gui.py
@@ -1,13 +1,51 @@
 import PySimpleGUI as gui
+from modules import functions
 
 label = gui.Text("TODO")
-input_box = gui.InputText(tooltip="Enter your Todo")
+
+input_box = gui.InputText(tooltip="Enter your Todo",key='todo')
+list_box = gui.Listbox(values= functions.read_todos('todos.txt'),key='todos',enable_events=True,size=[45,10])
+
+#Buttons
 add_button = gui.Button("Add")
 edit_button = gui.Button("Edit")
 complete_button = gui.Button('Done')
 exit_button = gui.Button('Exit')
 
-app_window = gui.Window('My Todo App',layout=[[label],[input_box,add_button],[edit_button , complete_button, exit_button]])
-app_window.read()
-print('app loaded done')
+
+
+app_window = gui.Window('My Todo App',layout=[[label],[input_box,add_button],[list_box,edit_button ],[complete_button, exit_button]],font=('Helvetica', 20))
+
+
+while True:
+    event,values = app_window.read()
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+            app_window['todos'].update(values=todos)
+
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            app_window['todos'].update(values=todos)
+
+
+            
+        case gui.WIN_CLOSED:
+            break
+
+
+
+
+
 app_window.close()
+
+
--- PYTHON_APPS/TODO_APP_GUI/modules/functions.py
+++ PYTHON_APPS/TODO_APP_GUI/modules/functions.py
@@ -1,9 +1,34 @@
-def read_todos(filepath):
-    with open(filepath,'r') as file:
-        todos = file.readlines()
-        return todos
+FILEPATH = "todos.txt"
 
-def write_todos(filepath,todos):
-    with open(filepath,'w') as file:
-        file.writelines(todos)
 
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+def read_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+
+    print(get_todos())
