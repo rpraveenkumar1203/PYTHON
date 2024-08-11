612879e870506f1972551ae4fb0cfc5375a00093
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-11 17:05:48 +0530

    crearted webapp[]

--- PYTHON_APPS/Todo_web_app/web-app.py
+++ PYTHON_APPS/Todo_web_app/web-app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+from modules import functions
 
+todos = functions.get_todos()
 
 st.title("My Todo for the the day ")
-st.write("in this app we can add, edit and mark as done the todo")
+st.write("In this app we can add, edit and mark as done the todo")
+
+
+for todo in todos :
+    st.checkbox(todo)
+
