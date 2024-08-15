55d693f3d19f92e2cdb7f2af5042154a17ddfa77
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-15 12:36:07 +0530

    Created a  multicolumn website and uploaded the projects descripton

---
+++ PYTHON_APPS/Portfolio/app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+
+image = "images/rPraveenKumar.jpg"
+descrpiton = '''🌟 Enthusiastic developer exploring Python, Golang, AI, and Machine Learning. Focused on clean, efficient code and always learning and innovating. 🚀'''
+
+st.set_page_config(layout='wide')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image(image=image,width=300)
+
+with col2 :
+    st.title("PraveenKumar Ramesh")
+    st.info(descrpiton)
+
+st.subheader('Below are the Apps developed by me from scratch')
+
+
+col3,empty_col,col4 = st.columns([1.5,0.5,1.5])
+
+df = pd.read_csv('data.csv',sep=';')
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Sourcecode]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+        st.write(row['description'])
+        st.image('images/' + row['image'])
+        st.write(f"[Sourcecode]({row['url']})")
+
