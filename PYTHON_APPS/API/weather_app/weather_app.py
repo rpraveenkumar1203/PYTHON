22ffe3c8a6dcadb9e1a5864f300011e79cbfab07
rpraveenkumar1203 <rpraveenkumar1203@outlook.com>
2024-08-16 22:27:59 +0530

    created weather api APP

--- PYTHON_APPS/API/weather_app/weather_app.py
+++ PYTHON_APPS/API/weather_app/weather_app.py
@@ -1,21 +1,21 @@
-from flask import Flask , render_template
+from flask import Flask ,render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def home():
+
+def home_page():
     return render_template('home.html')
 
 @app.route('/v1/api/<station>/<date>')
-def api_data(station,date):
-    temp = 46
-    return {'station': station,'date':date,'temperature':temp}
 
+def weather_api_help_page(station,date):
+    data = 'weather_data/data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(data,skiprows=20,parse_dates=['    DATE'])
+    temp = df.loc[df['    DATE']==date]['   TG'].squeeze()/10
+    #print(temp)
+    return {'date':date,'station':station,'temperature':temp}
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
